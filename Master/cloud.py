2e52b8d1a095746f6f1aeea002239eed800df9df
Sebastian Streckfuss <bla.sahbachung@gmal.com>
2020-05-22 15:52:25 +1000

    update

--- Master/cloud.py
+++ Master/cloud.py
@@ -1,31 +1,39 @@
 import os
 import pickle
+import json
 
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 from google.auth.transport.requests import Request
 
 
+def build_credentials(path) -> dict:
+    with open(path, "rb") as creds:
+        return json.load(creds)
+
+
 class CloudCursor:
 
     SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
     creds = None
+    credentials = {}
     service = None
 
     def __init__(self, **kwargs):
         self.service = self.get_service(**kwargs)
+        self.credentials = build_credentials(kwargs.get("credentials", "Master/credentials.json"))
 
     def get_service(self, **kwargs):
         if os.path.exists(kwargs.get("token", "Master/cloud_token.pickle")):
             with open(kwargs.get("token", "Master/cloud_token.pickle"), "rb") as token:
                 self.creds = pickle.load(token)
         if not self.creds:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
+            if self.creds and self.creds.refresh_token:
                 self.creds.refresh(Request())
-            else:
-                self.creds = ServiceAccountCredentials.from_json_keyfile_name(
-                    filename=kwargs.get("credentials", "Master/credentials.json"),
-                    scopes=self.SCOPES)
+            elif self.credentials:
+                self.creds = ServiceAccountCredentials.from_json_keyfile_dict(
+                    self.credentials, scopes=self.SCOPES
+                )
         return build("cloud_sql", "v3", credentials=self.creds)
 
     def execute(self, q):...
